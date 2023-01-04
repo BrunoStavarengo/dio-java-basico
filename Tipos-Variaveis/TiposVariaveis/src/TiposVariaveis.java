aa45be16a67484c35a4e94a6b948c8498db82273
BrunoStavarengo <brunostavarengo12@gmail.com>
2023-01-04 12:24:25 -0300

    resumo dos tipos de variaveis

--- Tipos-Variaveis/TiposVariaveis/src/TiposVariaveis.java
+++ Tipos-Variaveis/TiposVariaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+
+        boolean verdadeiro; //Só admite valores true ou false
+        char caractere; //Usa código UNICODE e ocupa cada caractere 16bits
+        String texto; //Armazenar textos(numeros ou caracteres)
+        
+        //Numeros inteiros
+        byte numero1; //1 byte
+        short numero2; //2 bytes
+        int numero3; //4 bytes 
+        long numero4; //8 bytes
+
+        //Numeros reais
+        float real; //4 bytes
+        double real1; //8 bytes
     }
 }
